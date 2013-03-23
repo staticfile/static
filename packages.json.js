ec5b67a81053245b7d9384decd140d9cf9c59ebd
Lachlan Collins <lockyc@gmail.com>
2013-03-23 13:11:46 +1000

    asdf

--- packages.json.js
+++ packages.json.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 
 var packages = Array();
 
-glob("../cdnjs/ajax/libs/**/package.json", function (error, matches) {
+glob("ajax/libs/**/package.json", function (error, matches) {
   matches.forEach(function(element){
     var package = JSON.parse(fs.readFileSync(element, 'utf8'));
     package.assets = Array();
-    var versions = glob.sync("../cdnjs/ajax/libs/"+package.name+"/!(package.json)");
+    var versions = glob.sync("ajax/libs/"+package.name+"/!(package.json)");
     versions.forEach(function(version) {
       var temp = Object();
       temp.version = version.replace(/^.+\//, "");
@@ -21,5 +21,5 @@ glob("../cdnjs/ajax/libs/**/package.json", function (error, matches) {
     packages.push(package);
   });
 
-  fs.writeFileSync('../cdnjs-website/packages.json', JSON.stringify({"packages":packages}, null, 4), 'utf8');
+  fs.writeFileSync('packages.json', JSON.stringify({"packages":packages}, null, 4), 'utf8');
 });
