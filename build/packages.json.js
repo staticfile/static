a105bdeb157d0ab9590b3e44febdcde4804f4d1f
Thomas Davis <thomasalwyndavis@gmail.com>
2013-03-25 12:59:45 +0000

    modified packages

--- build/packages.json.js
+++ build/packages.json.js
@@ -1,5 +1,6 @@
 var glob = require('glob');
 var fs = require('fs');
+var _ = require('underscore');
 var natcompare = require('./natcompare.js');
 var RSS = require('rss');
 var feed = new RSS({
@@ -15,7 +16,55 @@ var feed = new RSS({
         link:     'https://example.com/john-doe'
     }
 });
+var exec=require('child_process').exec;
+exec('git ls-tree -r --name-only HEAD | grep **/package.json | while read filename; do   echo "$(git log -1 --since="2 weeks ago" --name-status --format="%ad" -- $filename) blahcrap"; done',function(err,stdout,stderr){
+    var recentLibraries = stdout.split('blahcrap');
+    recentLibraries = _.filter(recentLibraries, function(lib){
+      if(lib.length > 4) {
+        return true;
+      };
+      return false;
+    })
+    recentLibraries = _.map(recentLibraries, function(lib){
+      lib = lib.replace('\n\n', '\n');
+      lib = lib.replace('\t', '\n');
+      lib = lib.substr(1);
+      lib = lib.split('\n');
+      lib[0] = new Date(lib[0]);
+      lib = {
+        date: lib[0],
+        change: lib[1],
+        path: lib[2].replace(/(^\s+|\s+$)/g, '')
+      }
+      return lib;
+    })
+    recentLibraries = _.sortBy(recentLibraries, function(arrayElement) {
+    //element will be each array, so we just return a date from first element in it
+    return arrayElement.date.getTime();
+    });
+    recentLibraries = recentLibraries.reverse();
+    _.each(recentLibraries, function (lib) {
+      var package = JSON.parse(fs.readFileSync(lib.path, 'utf8'));
+      var title = '';
+      if(lib.change === 'M') {
+        title = package.name + ' was modified'
+      }
+      if(lib.change === 'A') {
+        title = package.name + ' was added'
+      }
+      feed.item({
+          title:          title,
+          link:           package.homepage,
+          description:    package.description,
+          date:           lib.date
+      });
+    })
+    fs.writeFileSync('rss', feed.xml(), 'utf8');
 
+    console.log(recentLibraries);
+})
+
+return;
 var packages = Array();
 
 glob("ajax/libs/**/package.json", function (error, matches) {
@@ -36,16 +85,8 @@ glob("ajax/libs/**/package.json", function (error, matches) {
       return natcompare.compare(a.version, b.version);
     })
     package.assets.reverse();
-    feed.item({
-        title:          package.name,
-        link:           package.homepage,
-        description:    package.description,
-        date:           new Date()
-    });
     packages.push(package);
   });
   // Initialize the feed object
-  console.log(feed.xml());
-  fs.writeFileSync('rss', feed.xml(), 'utf8');
   fs.writeFileSync('packages.json', JSON.stringify({"packages":packages}, null, 4), 'utf8');
 });
