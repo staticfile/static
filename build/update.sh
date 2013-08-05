e6bc9ddab90b33fdae3f7950dfbe88423440f609
hfcorriez <hfcorriez@gmail.com>
2013-08-05 21:43:34 +0800

    Support dispatch packages.json to directory

--- build/update.sh
+++ build/update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$1" == "" ]
+then
+    echo "[Usage] ./build/update.sh <dir>"
+    exit
+fi
+
 echo "- Update git"
 git submodule update --force
 git pull
@@ -8,3 +14,7 @@ rm -rf cdnjs
 cp -rf ref-cdnjs/ajax/libs cdnjs
 echo "- Build packages.json"
 node build/build.js
+echo "- Copy packages.json to $1"
+cp packages.json $1
+
+
