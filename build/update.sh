b7642a80f0e1023c006d39cbc8435ec99a1859c2
hfcorriez <hfcorriez@gmail.com>
2013-08-06 14:32:12 +0800

    根据平台判断cp策略

--- build/update.sh
+++ build/update.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+platform=`uname`
+
 echo "- Update git"
 git pull
 git submodule update
+
 echo "- Update dist"
-cp -ru ref-cdnjs/ajax/libs/* dist/
-cp -ru libs/* dist/
+mkdir -p dist/
+if [ "$platform" == "Linux" ];then
+    cp -ru ref-cdnjs/ajax/libs/* dist/
+    cp -ru libs/* dist/
+else
+    cp -rn ref-cdnjs/ajax/libs/* dist/
+    cp -rn libs/* dist/
+fi
+
 echo "- Build packages.json"
 node build/build.js ./dist
 if [ ! -z $1 ]; then
