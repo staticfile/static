4fa6bbc6b3a82e9263f5a40949c79fba026c0c37
hfcorriez <hfcorriez@gmail.com>
2013-08-06 14:03:46 +0800

    调整发布策略

--- build/update.sh
+++ build/update.sh
@@ -3,11 +3,9 @@
 echo "- Update git"
 git pull
 git submodule update
-echo "- Create dist"
-rm -rf dist
-mkdir -p dist
-cp -rf ref-cdnjs/ajax/libs/* dist/
-cp -rf libs/* dist/
+echo "- Update dist"
+cp -rn ref-cdnjs/ajax/libs/* dist/
+cp -rn libs/* dist/
 echo "- Build packages.json"
 node build/build.js ./dist
 if [ ! -z $1 ]; then
