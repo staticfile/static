9ea568e0cce36b695812dafdbf3fd161b3c16012
webberg <webbery@foxmail.com>
2017-07-05 17:03:04 +0800

    add bootstrap-markdown lib

---
+++ libs/bootstrap-markdown/2.10.0/bootstrap-markdown.es.js
@@ -0,0 +1,31 @@
+/**
+ * Spanish translation for bootstrap-markdown
+ * by Leandro Poblet <leandrodrhouse@gmail.com>
+ */
+(function ($) {
+  $.fn.markdown.messages.es = {
+    'Bold': "Negrita",
+    'Italic': "Itálica",
+    'Heading': "Título",
+    'URL/Link': "Inserte un link",
+    'Image': "Inserte una imagen",
+    'List': "Lista de items",
+    'Unordered List': "Lista desordenada",
+    'Ordered List': "Lista ordenada",
+    'Code': "Código",
+    'Quote': "Cita",
+    'Preview': "Previsualizar",
+    'strong text': "Texto importante",
+    'emphasized text': "Texto con énfasis",
+    'heading text': "Texto de título",
+    'enter link description here': "Descripción del link",
+    'Insert Hyperlink': "Inserte un hipervínculo",
+    'enter image description here': "Descripción de la imagen",
+    'Insert Image Hyperlink': "Inserte una imagen con un hipervínculo",
+    'enter image title here': "Inserte una imagen con título",
+    'list text here': "Texto de lista aquí",
+    'code text here': "Código aquí",
+    'quote here': "Cita aquí",
+    'Save': "Guardar"
+  };
+}(jQuery));
